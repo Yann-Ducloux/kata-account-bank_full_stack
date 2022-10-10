dac98e88b8c4d201c93d13d40010f183203d9672
Tandoori <ducloux.y@gmail.com>
2022-10-10 16:40:22 +0200

    ajout angular material

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -2,8 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthentificationComponent } from './authentification/authentification.component';
 import { CreateClientComponent } from './create-client/create-client.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
   {
     path: 'createClient',
     component: CreateClientComponent
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -2,23 +2,29 @@ import { HttpClientModule, } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatButtonModule } from '@angular/material/button'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CreateClientComponent } from './create-client/create-client.component';
 import { AuthentificationComponent } from './authentification/authentification.component';
-
+import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card'; 
 @NgModule({
   declarations: [
     AppComponent,
     CreateClientComponent,
     AuthentificationComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
     HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
